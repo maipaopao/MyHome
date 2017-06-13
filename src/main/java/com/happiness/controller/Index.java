34bfc2d248df75da9cc8432421b8313fef950970
Yang Rui <yangrui0418@126.com>
2017-06-13 09:28:55 +0800

    完善读取配置文件

--- src/main/java/com/happiness/controller/Index.java
+++ src/main/java/com/happiness/controller/Index.java
@@ -1,5 +1,6 @@
 package com.happiness.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,15 +9,31 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/")
 public class Index {
 
+    @Value( "${wechat.appkey:123}" )
+    private String appkey;
+    @Value( "${wechat.secret:123}" )
+    private String secret;
+    @Value( "${wechat.name:123}" )
+    private String name;
+    
+    @Value("#{appProperties['Profile.pppas']}")
+    private String pppas;
+    @Value("${Profile.pppas2}")
+    private String pppas2;
+    
     @RequestMapping("/index")
     public String index(){
+        
+        System.out.println(appkey);
+        System.out.println(secret);
+        
         return "index";
     }
     
     @RequestMapping("/login")
     @ResponseBody
     public String login(){
-        return "login";
+        return "appkey=" + appkey + ",secret=" + secret + ",name=" + name + ",pppas=" + pppas + ",pppas2=" + pppas2;
     }
     
 }
