b1dcdb6340a88adde615304d6e30cd2aaaecb545
Yang Rui <yangrui0418@126.com>
2017-06-14 11:28:20 +0800

    修改log路径

--- src/main/java/com/happiness/core/config/RedisConfig.java
+++ src/main/java/com/happiness/core/config/RedisConfig.java
@@ -70,20 +70,25 @@ public class RedisConfig {
         jedisConnectionFactory.setPort(redisPort);
         jedisConnectionFactory.setPassword(redisPassword);
         JedisPoolConfig poolConfig = new JedisPoolConfig();
+        //获取连接时的最大等待毫秒数(如果设置为阻塞时BlockWhenExhausted),如果超时就抛异常, 小于零:阻塞不确定的时间,  默认-1
         poolConfig.setMaxWaitMillis(redisMaxWaitMillis);
         //最大连接数, 默认8个
         poolConfig.setMaxTotal(redisMaxTotal);
         //最大空闲连接数, 默认8个
         poolConfig.setMaxIdle(redisMaxIdle);
+        //是否启用后进先出, 默认true
         poolConfig.setLifo(true);
+        //逐出连接的最小空闲时间 默认1800000毫秒(30分钟)
         poolConfig.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        //最小空闲连接数, 默认0
         poolConfig.setMinIdle(redisMinIdle);
-        //逐出扫描的时间间隔(毫秒) 
+        //逐出扫描的时间间隔(毫秒) 如果为负数,则不运行逐出线程, 默认-1
         poolConfig.setTimeBetweenEvictionRunsMillis(redisEvictionRunTime);
-        //对象空闲多久后逐出, 当空闲时间>该值 且 空闲连接>最大空闲数
+        //对象空闲多久后逐出, 当空闲时间>该值 且 空闲连接>最大空闲数 时直接逐出,不再根据MinEvictableIdleTimeMillis判断  (默认逐出策略)
         poolConfig.setSoftMinEvictableIdleTimeMillis(redisEvictableIdleTime);
         //在获取连接的时候检查有效性, 默认false
         poolConfig.setTestOnBorrow(true);
+        //在空闲时检查有效性, 默认false
         poolConfig.setTestWhileIdle(true);
         //每次逐出检查时 逐出的最大数目
         poolConfig.setNumTestsPerEvictionRun(redisMaxIdle / 2);
